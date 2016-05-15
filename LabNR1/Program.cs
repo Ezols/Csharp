567252add28be27ed7661e39979cff6a7c0929ba
Ozols <its27eo1@odin.local>
2016-05-15 22:01:08 +0300

    Shape - Inheritance, Polymorfism, Encapsulation

--- LabNR1/Program.cs
+++ LabNR1/Program.cs
@@ -6,21 +6,52 @@ using System.Threading.Tasks;
 
 namespace Shape
 {
+    abstract class Shape
+    {
+        abstract public double Area();
+        abstract public double Perimeter();
+        public double AreaDiff(Shape rectangle)
+        {
+            return this.Area() - rectangle.Area();
+        }
+    }
+
+    class Circle : Shape
+    {
+        protected double diameter { get; set; }
+        protected double perimeter { get; set; }
+
+        public Circle(double diameter)
+        {
+            this.diameter = diameter;
+            this.perimeter = diameter * Math.PI;
+        }
+
+       override public double Area()
+        {
+            return Math.PI * Math.Pow(this.diameter / 2, 2);
+        }
+
+       override public double Perimeter()
+        {
+            return this.perimeter;
+        }
+    }
     class Square : Rectangle
     {
-        public Square(int size) : base(size, size)
+        public Square(double size) : base(size, size)
         {
           
         }
     }
-    class Rectangle
+    class Rectangle : Shape
     {
-        protected int height { set; get; }
-        protected int width { set; get; }
-        protected int perimeter { set; get; }
-        protected int area { get; set; }
+        protected double height { set; get; }
+        protected double width { set; get; }
+        protected double perimeter { set; get; }
+        protected double area { get; set; }
 
-        public Rectangle(int height, int width)
+        public Rectangle(double height, double width)
         {
             this.height = height;
             this.width = width;
@@ -28,15 +59,15 @@ namespace Shape
             this.area = height * width;
         }
 
-        public int Area() 
+        override public double Area() 
         {
             return this.area;
         }
 
-        public int Perimeter()
+        override public double Perimeter()
         {
             return this.perimeter;
-        }
+        }        
     }
 
     class Program
@@ -46,12 +77,21 @@ namespace Shape
             Rectangle rectangle = new Rectangle(10, 30); //instances
             Rectangle rectangle2 = new Rectangle(5, 15);
             Console.WriteLine("Renctagle area = " + rectangle.Area());
-            Console.WriteLine("Renctangle perimeter = " + rectangle2.Perimeter());
+
+            Console.WriteLine("Renctangle area = " + rectangle2.Area());
+            Console.WriteLine("Area diff. " + rectangle.AreaDiff(rectangle2));
+            Console.WriteLine("Area diff. " + rectangle2.AreaDiff(rectangle));
 
             Square square = new Square(10);
             Square square2 = new Square(5);
             Console.WriteLine("Square area = " + square.Area());
             Console.WriteLine("Square2 perimeter = " + square2.Perimeter());
+
+            Circle circle = new Circle(20);
+            Console.WriteLine("Area of circle = " + circle.Area());
+            Console.WriteLine("Perimeter of circle = " + circle.Perimeter());
+
+            Console.WriteLine("Rectangel and Circle area diff. " + rectangle.AreaDiff(circle)); // Polymorfism
         }
     }
 }
